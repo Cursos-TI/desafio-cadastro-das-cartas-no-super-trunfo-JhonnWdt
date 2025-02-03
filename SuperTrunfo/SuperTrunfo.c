9804ca4de4b1446dd7d434dbb362df02840a0c2b
JhonnWdt <joniwendt46@gmail.com>
2025-02-03 23:40:09 +0000

    notas adicionadas

--- SuperTrunfo/SuperTrunfo.c
+++ SuperTrunfo/SuperTrunfo.c
@@ -15,7 +15,7 @@ int main() {
     printf("População: \n");
     scanf("%d", &populacao);
 
-    printf("PIB: \n");              
+    printf("PIB: \n");           // os elementos %d, %f, e %s, indicam quais tipos de dados podem ser inseridos, como números inteiros (%d), dados de números com casas decimais, chamados de float(%f). ou strings, que são os tipos de dados que armazenam textos (%s).
     scanf("%f", &pib);
 
     printf("Número de Pontos Turísticos: \n");
@@ -25,8 +25,8 @@ int main() {
     scanf("%f", &area);
 
     printf("\n=== Dados da Cidade ===\n");
-    printf("Nome: %s \n", nome);
-    printf("População: %d \n", populacao);
+    printf("Nome: %s \n", nome);                               // aqui, é definido como a informação será impressa na tela através do comando printf. 
+    printf("População: %d \n", populacao);                      
     printf("PIB: %f \n", pib);
     printf("Número de Pontos Turísticos: %d \n", turismo);
     printf("Área da Cidade (km²): %f\n", area);
